a9b88a615ef82b4962c5a4c508ecffdb0cf2d744
Stefan Ruschke <stefan.ruschke@tum.de>
2019-04-16 10:11:37 +0200

    added info

--- tests/runall_tests.m
+++ tests/runall_tests.m
@@ -1,5 +1,30 @@
 %% Run all tests
 % Helper to run all exisiting tests located under ./tests/
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Created:	Apr 16, 2019
+%
+% Revisions:    0.1 (Apr 16, 2019)
+%					Initial version.
+%
+% Authors: 
+%
+%   stefan.ruschke@tum.de
+% 
+% --------------------------------
+%
+% Body Magnetic Resonance Research Group
+% Department of Diagnostic and Interventional Radiology
+% Technical University of Munich
+% Klinikum rechts der Isar
+% 22 Ismaninger St., 81675 Munich
+% 
+% https://www.bmrr.de
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 import matlab.unittest.TestSuite;
 
