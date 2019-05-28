ddf868bddbbd8aa2a0bd4a99f491ad07f0862237
Stefan Ruschke <stefan.ruschke@tum.de>
2019-05-28 10:11:43 +0200

    added meannd

---
+++ meannd.m
@@ -0,0 +1,32 @@
+function [ Y ] = meannd( X , DIM )
+%MEANND mean along multiple dimensions.
+%   Detailed explanation goes here
+%
+%   See also maxnd, minnd
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Created:	Apr 15, 2019
+%
+% Revisions:    0.1 (Apr 15, 2019)
+%					Initial version.
+%
+% Authors: 
+%
+%   stefan.ruschke@tum.de
+% 
+% --------------------------------
+%
+% Body Magnetic Resonance Research Group
+% Department of Diagnostic and Interventional Radiology
+% Technical University of Munich
+% Klinikum rechts der Isar
+% 22 Ismaninger St., 81675 Munich
+% 
+% https://www.bmrr.de
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Y = fhnd( @(x,d) mean(x,d), X, DIM);
+
+end
